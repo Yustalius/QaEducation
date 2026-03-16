c14a075294fa8e5792d4596970610f005f7240d4
ofyks <kiruxa2011liverpool@gmail.com>
2026-03-16 19:46:19 +0300

    Добавлены тесты с паттерном Page Object для GitHub и Bulgakov

--- src/test/java/com/qa/github/NameSearch.java
+++ src/test/java/com/qa/github/NameSearch.java
@@ -1,44 +1,67 @@
 package com.qa.github;
 
-import com.codeborne.selenide.Configuration;
+import com.qa.pages.GitHubMainPage;
+import com.qa.pages.YandexResultPage;
+import com.qa.pages.YandexSearchPage;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class NameSearch {
     @Test
     void checkRepositories() {
-        /*
-         * Тест-кейс - проверить, что имя хозяина репы getting-started - Владислав Юстус
-         * 1. открыть поисковик
-         * 2. ввести данные сайта - Git Hub
-         * 3. нажать кнопку поиск
-         * 4. в поисковой выдаче найти нужный сайт, кликнуть на него
-         * 5. нажать на поиск вправом верхнем углу
-         * 6. Вставить в поиске "java-automation-qa/getting-started"
-         * 7. нажать на Search all of GitHub
-         * 8. Кликнуть на результат из выдачи
-         * 9. Проверить, что имя владельца репы Владислав Юстус
-         * */
-
-        //Configuration.holdBrowserOpen = true; // браузер не закрывается после проверки
-        Configuration.pageLoadTimeout = 100000; // таймаут прогрузги страницы
-        Configuration.timeout = 100000; // таймаут прогрузги элементов
-
+        YandexSearchPage yandexSearchPage = new YandexSearchPage();
+        YandexResultPage yandexResultPage = new YandexResultPage();
+        GitHubMainPage gitHubMainPage = new GitHubMainPage();
         open("https://ya.ru/");
-        $("#text").setValue("Git Hub");
-        $(".search3__button").click();
-        $(".DistributionButtonClose_view_cross").click();
-        $(byText("github.com")).click();
 
-        switchTo().window(1);
-        $(".header-search-button").click();
-        $("#query-builder-test").setValue("java-automation-qa/getting-started");
-        $(".ActionListItem-description").click();
-        $(".hkFRpV").click();
-        $$(".AuthorDisplayName-module__Text__OP5Q9").get(0).shouldHave(text("Владислав Юстус"));
+        yandexSearchPage
+                .search ("Git Hub")
+                .clickSearch ();
+
+        yandexResultPage
+                .closeDefaultWindow ()
+                .openSiteGitHub ();
+
+        gitHubMainPage
+                .clickSearchMenu ()
+                .searchTextMenu ("java-automation-qa/getting-started")
+                .clickButtonSearch ()
+                .openProjectGitHub ()
+                .checkNameOwner ();
 
     }
 }
+
+
+//$("#text").setValue("Git Hub"); ready
+//$(".search3__button").click(); ready
+//$(".DistributionButtonClose_view_cross").click();
+//$(byText("github.com")).click();
+//
+//switchTo().window(1);
+//$(".header-search-button").click(); ready
+//$("#query-builder-test").setValue("java-automation-qa/getting-started");
+//$(".ActionListItem-description").click();
+//$(".hkFRpV").click();
+//$$(".AuthorDisplayName-module__Text__OP5Q9").get(0).shouldHave(text("Владислав Юстус"));
+
+
+
+
+/*
+ * Тест-кейс - проверить, что имя хозяина репы getting-started - Владислав Юстус
+ * 1. открыть поисковик
+ * 2. ввести данные сайта - Git Hub
+ * 3. нажать кнопку поиск
+ * 4. в поисковой выдаче найти нужный сайт, кликнуть на него
+ * 5. нажать на поиск вправом верхнем углу
+ * 6. Вставить в поиске "java-automation-qa/getting-started"
+ * 7. нажать на Search all of GitHub
+ * 8. Кликнуть на результат из выдачи
+ * 9. Проверить, что имя владельца репы Владислав Юстус
+ * */
+
+//Configuration.holdBrowserOpen = true; // браузер не закрывается после проверки
+//Configuration.pageLoadTimeout = 100000; // таймаут прогрузги страницы
+//Configuration.timeout = 100000; // таймаут прогрузги элементов
--- src/test/java/ru/bulgakov/qa/SearchTest.java
+++ src/test/java/ru/bulgakov/qa/SearchTest.java
@@ -1,8 +1,14 @@
 package ru.bulgakov.qa;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import ru.bulgakov.pages.WelcomePage;
+import ru.bulgakov.pages.YandexResultsPage;
+import ru.bulgakov.pages.YandexSearchPage;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
@@ -12,36 +18,60 @@ import static com.codeborne.selenide.Selenide.*;
 public class SearchTest {
 
     @Test
+    @DisplayName("Проверить, что цена обучения 47 000 рублей")
     void mentoringPrice47000Test() {
-    /*
-    * Тест-кейс - проверить, что предоплата по обучению - 47000 рублей
-    * 1. открыть поисковие
-    * 2. ввести данные сайта
-    * 3. нажать кнопку поиск
-    * 4. в поисковой выдаче найти нужный сайт, кликнуть на него
-    * 5. нажать на кнопку стоимость
-    * 6. нажать на кнопку "Хочу вкатиться в QA"
-    * 7. нажать на кнопку "Бегу оплачивать"
-    * 8. проверить, что к оплате 47 000 рублей
-    * */
-        Configuration.holdBrowserOpen = true; // браузер не закрывается после проверки
-        Configuration.pageLoadTimeout = 100000; // таймаут прогрузги страницы
-        Configuration.timeout = 100000; // таймаут прогрузги элементов
-
-        open("https://ya.ru/");
-        $("#text").setValue("bulgakov qa");
-        $("[type=submit]").click();
-        $(".DistributionButtonClose").click();
-        $(byText("ivanbulgakovqa.ru")).click();
-
-        sleep(3000);
-        switchTo().window(1);
-        $$(".t-menu__list li").last().click();
-        $x("/html/body/div[1]/div[42]/div/div/div[32]/div/a").click();
-        $(byText("Бегу оплачивать")).click();
-
-        switchTo().window(2);
-        $(".styles-module-scss-module__t92_WG__price\n h2").shouldHave(text("₽ 47 000 "));
-
+        open("https://ya.ru/", YandexSearchPage.class)
+                .search("bulgakov qa")
+                .submit()
+                .closeDefaultBrowserWindow()
+                .openLink("ivanbulgakovqa.ru")
+                .clickToPrice()
+                .clickTryQaMentor()
+                .clickButtonTextRunPay()
+                .checkPrice();
     }
-}
+        }
+
+
+
+
+
+
+
+// создаем класс под страницу -
+// выписываем меиоды для взаимодействия -
+// вытаскиваем из теста дейстивя -
+// выносим локаторы в переменные -
+
+
+
+
+
+
+//        sleep(3000);
+//        switchTo().window(1);
+//        $$(".t-menu__list li").last().click();
+//        $x("/html/body/div[1]/div[42]/div/div/div[32]/div/a").click();
+//        $(byText("Бегу оплачивать")).click();
+//
+//        switchTo().window(2);
+//        $(".styles-module-scss-module__t92_WG__price\n h2").shouldHave(text("₽ 47 000 "));
+//
+//    }
+//}
+
+//        Configuration.holdBrowserOpen = true; // браузер не закрывается после проверки
+//        Configuration.pageLoadTimeout = 100000; // таймаут прогрузги страницы
+//        Configuration.timeout = 100000; // таймаут прогрузги элементов
+
+/*
+ * Тест-кейс - проверить, что предоплата по обучению - 47000 рублей
+ * 1. открыть поисковие
+ * 2. ввести данные сайта
+ * 3. нажать кнопку поиск
+ * 4. в поисковой выдаче найти нужный сайт, кликнуть на него
+ * 5. нажать на кнопку стоимость
+ * 6. нажать на кнопку "Хочу вкатиться в QA"
+ * 7. нажать на кнопку "Бегу оплачивать"
+ * 8. проверить, что к оплате 47 000 рублей
+ * */
